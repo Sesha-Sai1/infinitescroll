3a0b2ad22bafcd3764fd9eb86df53e5d8a13f392
Sesha-Sai1 <seshasaikothapalli1350@gmail.com>
2024-03-25 15:46:14 +0530

    second commit ignore added to src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 import InfiniteScrollDataSource from "./components/Infinite";
-import InfiniteScrollData from "./components/InfiniteScroll";
-import InfiniteScrollComponent from "./components/InfiniteScrolling";
 
 function App() {
   return (
     <div className="App">
-      {/* <InfiniteScrollData /> */}
-      {/* <InfiniteScrollComponent /> */}
       <InfiniteScrollDataSource />
     </div>
   );
